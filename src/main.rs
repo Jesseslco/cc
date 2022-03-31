276b5694675a51da5c5a9f1c7d740d3ad226c210
jesseslco <jesseslco@gmail.com>
2022-04-01 00:47:38 +0900

    add folder name encryption

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
+mod decrypt;
 #[allow(dead_code)]
 mod encrypt;
-mod decrypt;
-use encrypt::encode_to_blob;
 use decrypt::decrypt_blob;
+use encrypt::encode_to_blob;
 
 use clap::Parser;
+use std::fs::{self, File};
 use std::io::Write;
 use std::path::{Path, PathBuf};
-use std::fs::{self, File};
 use uuid::Uuid;
-
 
 #[allow(dead_code)]
 fn get_uuid4() -> String {
@@ -17,6 +16,15 @@ fn get_uuid4() -> String {
     return uuid.to_string();
 }
 
+#[allow(dead_code)]
+fn simple_encrypt_folder_name(folder_name: &str) -> String {
+    return folder_name.to_string();
+}
+
+#[allow(dead_code)]
+fn simple_decrypt_foler_name(encrypted_folder_name: &str) -> String {
+    return encrypted_folder_name.to_string();
+}
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -28,7 +36,7 @@ struct Args {
     src: String,
 
     #[clap(short, long)]
-    dst: String
+    dst: String,
 }
 
 #[derive(Debug)]
@@ -64,12 +72,11 @@ fn main() {
         dst_path.to_owned()
     };
 
-
     // validate src_directory_path
     if !src_path.exists() {
         println!("Source file or directory does not exist");
         return;
-    } 
+    }
 
     // validate dst_path
     if !dst_path.exists() {
@@ -88,17 +95,15 @@ fn main() {
     match command {
         "encrypt" => {
             encrypt_folder(&src_path, &dst_path).expect("failed to encrypt folder");
-        },
+        }
         "decrypt" => {
             decrypt_folder(&src_path, &dst_path).expect("failed to decrypt folder");
-        },
+        }
         _ => {
             println!("Unknown command: {}", command);
         }
     }
-
 }
-
 
 // get file or directory path recrusively from a path
 fn list_dir(src: &Path, mut results: Vec<PathBuf>) -> Vec<PathBuf> {
@@ -132,15 +137,18 @@ fn encrypt_folder(src_path: &Path, dst_path: &Path) -> Result<(), Box<dyn std::e
 
     if files_in_directory.len() == 0 {
         println!("No files found in source directory");
-        return Err(Box::new(WtfError("No files found in source directory".to_string())));
+        return Err(Box::new(WtfError(
+            "No files found in source directory".to_string(),
+        )));
     }
-
 
     // create target directory
     let target_dir_path = dst_path.join(src_path.file_name().unwrap());
     if target_dir_path.exists() {
         println!("Target directory already exists");
-        return Err(Box::new(WtfError("Target directory already exists".to_string())));
+        return Err(Box::new(WtfError(
+            "Target directory already exists".to_string(),
+        )));
     } else {
         fs::create_dir(&target_dir_path).expect("failed to create target directory");
     }
@@ -148,17 +156,19 @@ fn encrypt_folder(src_path: &Path, dst_path: &Path) -> Result<(), Box<dyn std::e
     let meta_dir_path = target_dir_path.join(".meta");
     if meta_dir_path.exists() {
         println!("Meta directory already exists");
-        return Err(Box::new(WtfError("Meta directory already exists".to_string())));
+        return Err(Box::new(WtfError(
+            "Meta directory already exists".to_string(),
+        )));
     } else {
         fs::create_dir(&meta_dir_path).expect("failed to create meta directory");
     }
 
     for file_path in files_in_directory.iter() {
-        let file_blob = encode_to_blob(file_path,src_path.to_owned());
+        let file_blob = encode_to_blob(file_path, src_path.to_owned());
         let new_file_path = meta_dir_path.join(get_uuid4());
 
         write_content_to_file(&new_file_path, &file_blob);
-        
+
         println!("file_blob: {:?}", file_path);
     }
     Ok(())
@@ -168,7 +178,9 @@ fn decrypt_folder(src_path: &Path, dst_path: &Path) -> Result<(), Box<dyn std::e
     let meta_path = src_path.join(".meta");
     if !meta_path.exists() {
         println!("Meta directory does not exist");
-        return Err(Box::new(WtfError("Meta directory does not exist".to_string())));
+        return Err(Box::new(WtfError(
+            "Meta directory does not exist".to_string(),
+        )));
     }
 
     let project_name = src_path.file_name().unwrap().to_str().unwrap();
@@ -186,7 +198,8 @@ fn decrypt_folder(src_path: &Path, dst_path: &Path) -> Result<(), Box<dyn std::e
 
         // create file parent path if not exists
         if !file_path.parent().unwrap().exists() {
-            fs::create_dir_all(file_path.parent().unwrap()).expect("failed to create file parent directory");
+            fs::create_dir_all(file_path.parent().unwrap())
+                .expect("failed to create file parent directory");
         }
         write_content_to_file(&file_path, &file_content);
     }
